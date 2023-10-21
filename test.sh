3b482cb23ed202fc0c714491f1c8ec0742e0d82b
Satoshi Kataoka <k.3104is@gmail.com>
2023-10-21 20:32:26 +0900

    working

--- test.sh
+++ test.sh
@@ -1,50 +1,52 @@
 #!/bin/bash
 
+# comparison dir
+DIR1="./a"
+DIR2="./b"
+
 # patterns
 PTN1="^\."
-PTN2="(\.c|\.h)"
+MATCH_PTN="^.*(\.c|\.h|\.txt) *$"
+MISMATCH_PTN="^.*(cfg|integ).*$"
 
 # search gpj on ./a
-for gpj in $(find ./a -type f | grep "gpj")
+for gpj in $(find ${DIR1} -type f | grep -E "gpj$")
 do
+  # create gpj.bak on same dir
+  DIR_GPJBAK="${gpj%/*}/gpj.bak"
+  rm ${DIR_GPJBAK} > /dev/null 2>&1
+  touch ${DIR_GPJBAK}
+
   # path relative from gpj to pj(./)
   GPJ_RELATIVE_PATH=$(realpath --relative-to=$(dirname ${gpj}) ./)
   echo ${GPJ_RELATIVE_PATH}
-  # chk gpj file
+
+  # expand gpj file
   for line in $(cat ${gpj})
   do
-    if echo ${line} | grep -Eq ${PTN2}; then :; else echo ${line}" is false"; fi
-    echo ${line}
+    # extract .c/.h/.txt
+    # if echo ${line} | grep -Eq ${PTN2}; 
+    if [[ ${line} =~ ${MATCH_PTN} && ! ${line} =~ ${MISMATCH_PTN} ]] 
+    then
+      :
+    else
+      echo ${line} >> ${DIR_GPJBAK}
+      continue
+    fi
+
     # search src file on ./b
-    file2=$(find ./b -type f | grep ${line##*/})
-    echo ${file2}
-    echo "### change path to ./b ###"
-    echo ${file2} | sed -e 's,${PTN1},${GPJ_RELATIVE_PATH},g'
-    echo ${file2//${PTN1}/${GPJ_RELATIVE_PATH}}
+    file=$(find ${DIR2} -type f | grep ${line##*/})
+    # found
+    if [ -n "${file}" ];
+    then 
+      echo ${file/./${GPJ_RELATIVE_PATH}} >> ${DIR_GPJBAK}
+    # not found
+    else
+      echo "\`${line}\` is not found" >> ${DIR_GPJBAK}
+    fi
   done
+  echo "### cat ${DIR_GPJBAK} ###"
+  cat ${DIR_GPJBAK}
+  rm ${DIR_GPJBAK} > /dev/null 2>&1
+
 done
-
-
-echo "#######"
-x=1234
-echo $x | grep -E "^[0-9][0-9]*$" > /dev/null 2>&1
-if [ $? -eq 0 ]; then echo "x is number"; else echo "x is not number"; fi
-# if [ $? -eq 0 ]; then continue; fi
-echo "abcde" | grep -q "abc" && echo "true"
-
-
-echo "#######"
-
-for file1 in $(find ./a -type f | grep "txt")
-do 
-  echo ${file1}
-  file2=$(find ./b -type f | grep $(echo ${file1} | gawk -F/ '{print $NF}'))
-  echo ${file2}
-  realpath --relative-to=${file1} ./
-done
-
-F1=$(find  $(pwd) -type f -name "aaa.txt")
-F2=$(find  $(pwd) -type f -name "bbb.txt")
-RELATIVE_PATH=$(realpath --relative-to="$(dirname "$F1")" "$F2")
-#RELATIVE_PATH=$(realpath -q $F1 $F2)
-echo $RELATIVE_PATH
